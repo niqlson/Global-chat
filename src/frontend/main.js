ba88755650afdab7532d5e5835f301ede6ad62e5
AleksD1203 <smiley3223337@gmail.com>
2025-06-02 17:39:42 +0300

    Add generating id

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -1,31 +1,36 @@
-'use strict';
-
 const input = document.getElementById('messageInput');
 const chat = document.getElementById('chat');
 
-const insertMessage = (message) => {
+const putMessage = (message) => {
   const messageDiv = document.createElement('div');
   messageDiv.className = 'message my-message';
   messageDiv.textContent = message;
-  chat.appendChild(messageDiv);
-};
-
 const clearInput = () => {
-  input.value = '';
   chat.scrollTop = chat.scrollHeight;
 };
 
-const sendMessage = (message) => {
+const insertMessage = (message) => {
   const text = message.trim();
   if (text.length === 0) return;
-  insertMessage(message);
+  putMessage(message);
   clearInput();
 };
 
 document.getElementById('sendButton').addEventListener('click', () => {
-  sendMessage(input.value);
+  insertMessage(input.value);
 });
 
 input.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') sendMessage(input.value);
+  if (event.key === 'Enter') insertMessage(input.value);
 });
+
+const generateRandomKey = (length = 16) => {
+  const random = crypto.getRandomValues(new Uint8Array(length));
+  const hex = [...random].map((n) => n.toString(16).padStart(2, '0'));
+  return hex.join('');
+};
+
+(() => {
+  if (localStorage.getItem('id') !== null) return;
+  localStorage.setItem('id', generateRandomKey());
+})();
